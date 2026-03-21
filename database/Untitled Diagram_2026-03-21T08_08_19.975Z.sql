d34c7bd90d04ee7d9ca8a989e390a972d849c5a5
karplyukksenia <karplyukksenia@yandex.ru>
2026-03-21 11:31:23 +0300

    Убрали комментарии с таблицы

--- database/Untitled Diagram_2026-03-21T08_08_19.975Z.sql
+++ database/Untitled Diagram_2026-03-21T08_08_19.975Z.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS `Students` (
 	`id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
-	`skills` JSON COMMENT 'JSON - т.к. хранение массивов, которых нет в MySql (но мы все ещё можем поменять диалект sql)',
-	`university` VARCHAR(255) NOT NULL COMMENT 'поставила not null, если нет вуза (школьник) пусть будет категория школьник',
-	`faculty` VARCHAR(255) COMMENT 'а тут уже наверное можно и пустое поле оставлять, не настолько важная информация',
-	`course` TINYINT NOT NULL COMMENT 'хз насчет разделения бакалавриата/магистратуры, возможно стоит сделать VARCHAR',
+	`skills` JSON,
+	`university` VARCHAR(255) NOT NULL,
+	`faculty` VARCHAR(255),
+	`course` TINYINT NOT NULL,
 	`email` VARCHAR(255) NOT NULL UNIQUE CHECK(*@*),
 	`phone_number` VARCHAR(255) NOT NULL UNIQUE,
-	`tg_id` VARCHAR(255) UNIQUE COMMENT 'допустим, у кого-то нет тг, пусть будет возможность null',
-	`tasks_started` JSON COMMENT 'может быть стоит сделать два разных столбца под активные задания и под завершенные',
+	`tg_id` VARCHAR(255) UNIQUE,
+	`tasks_started` JSON,
 	`tasks_progressing` JSON,
 	`password_hash` VARCHAR(255) NOT NULL,
 	PRIMARY KEY(`id`)
@@ -18,7 +18,7 @@ CREATE TABLE IF NOT EXISTS `Companies` (
 	`id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
 	`name` VARCHAR(255) NOT NULL,
 	`informatinon` TEXT(65535),
-	`projects` JSON NOT NULL COMMENT 'может быть стоит сделать два разных столбца под активные задания и под завершенные',
+	`projects` JSON NOT NULL,
 	PRIMARY KEY(`id`)
 );
 
@@ -26,11 +26,11 @@ CREATE TABLE IF NOT EXISTS `Companies` (
 CREATE TABLE IF NOT EXISTS `Cases` (
 	`id` INTEGER NOT NULL AUTO_INCREMENT UNIQUE,
 	`organizer_id` INTEGER NOT NULL,
-	`performers` JSON COMMENT 'считаем, что один кейс может выполнять одновременно несколько людей, как проверять соотношение человек/кейс будем я пока не понимаю',
+	`performers` JSON,
 	`description` TEXT(65535) NOT NULL,
-	`areas` JSON COMMENT 'я ориентируюсь на картинку (https://t.me/c/2553619342/8974/18797)',
-	`publication_time` DATE NOT NULL COMMENT 'когда выложили задание',
-	`end_time` DATE NOT NULL COMMENT 'дедлайн, возможно у кого-то может быть без него, поэтому с возможностью null',
+	`areas` JSON,
+	`publication_time` DATE NOT NULL,
+	`end_time` DATE NOT NULL,
 	PRIMARY KEY(`id`)
 );
 
