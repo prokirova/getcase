eb6a95e25372ae725c983dc0d9af67ef2ec0c066
Karplyuk Ksenia <karplyukksenia@yandex.ru>
2026-03-21 16:44:35 +0300

    add register and login

--- main.py
+++ main.py
@@ -3,10 +3,77 @@ import os
 from datetime import datetime
 import bcrypt
 
+import mysql.connector
+
+def get_server_connection():
+    return mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='password'
+    )
+
+
+def init_db():
+    conn = get_server_connection()
+    cur = conn.cursor()
+
+    # создаем базу данных, если её нет
+    cur.execute("CREATE DATABASE IF NOT EXISTS GetCase")
+    cur.execute("USE GetCase")
+
+    # Students
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS Students (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        last_name VARCHAR(255) NOT NULL,
+        first_name VARCHAR(255) NOT NULL,
+        middle_name VARCHAR(255),
+        email VARCHAR(255) NOT NULL UNIQUE,
+        phone_number VARCHAR(255) NOT NULL UNIQUE,
+        password_hash VARCHAR(255) NOT NULL,
+        university VARCHAR(255) NOT NULL,
+        faculty VARCHAR(255) NOT NULL,
+        specialty VARCHAR(255) NOT NULL,
+        course TINYINT NOT NULL,
+        birthdate DATE NOT NULL,
+        skills JSON,
+        tg_id VARCHAR(255) UNIQUE,
+        tasks_started JSON,
+        tasks_progressing JSON
+    )
+    """)
+
+    # Companies
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS Companies (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        information TEXT,
+        projects JSON
+    )
+    """)
+
+    # Cases
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS Cases (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        organizer_id INT NOT NULL,
+        performers JSON,
+        description TEXT NOT NULL,
+        areas JSON,
+        publication_time DATE NOT NULL,
+        end_time DATE NOT NULL,
+        FOREIGN KEY (organizer_id) REFERENCES Companies(id) ON DELETE RESTRICT ON UPDATE CASCADE
+    )
+    """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
 
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
-#app.secret_key = os.getenv('FLASK_SECRET_KEY', 'fallback_secret_key_if_not_set')
+app.secret_key = os.getenv('FLASK_SECRET_KEY', 'fallback_secret_key_if_not_set')
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -14,41 +81,97 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        username = request.form['username']
+
+        last_name = request.form['last_name']
+        first_name = request.form['first_name']
+        middle_name = request.form.get('middle_name', '')
+
         email = request.form['email']
         password = request.form['password']
 
-        # Проверка длины пароля
+        university = request.form['university']
+        faculty = request.form['faculty']
+        specialty = request.form['specialty']
+        course = int(request.form['course'])
+
+        phone_number = request.form['phone']
+        birthdate = request.form['birthdate']
+
+        # проверки
         if len(password) < 6:
             flash('Пароль должен содержать не менее 6 символов')
             return render_template('register.html')
 
-        """
-        conn = get_db_connection()
-        cur = conn.cursor()
+        try:
+            db = get_database()
+            cur = db.cursor()
 
-        cur.execute("SELECT id FROM users WHERE email = ?", (email,))
-        if cur.fetchone():
-            flash('Пользователь с таким email уже существует')
+            # проверка email
+            cur.execute("SELECT id FROM Students WHERE email = %s", (email,))
+            if cur.fetchone():
+                flash('Пользователь с таким email уже существует')
+                return render_template('register.html')
+
+            # проверка телефона
+            cur.execute("SELECT id FROM Students WHERE phone_number = %s", (phone_number,))
+            if cur.fetchone():
+                flash('Пользователь с таким телефоном уже существует')
+                return render_template('register.html')
+
+            # хеш пароля
+            password_hash = bcrypt.hashpw(
+                password.encode('utf-8'),
+                bcrypt.gensalt()
+            ).decode('utf-8')
+
+            # вставка
+            cur.execute("""
+                INSERT INTO Students (
+                    last_name, first_name, middle_name,
+                    email, phone_number, password_hash,
+                    university, faculty, specialty, course,
+                    birthdate,
+                    skills, tg_id, tasks_started, tasks_progressing
+                )
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+            """, (
+                last_name,
+                first_name,
+                middle_name,
+
+                email,
+                phone_number,
+                password_hash,
+
+                university,
+                faculty,
+                specialty,
+                course,
+
+                birthdate,
+
+                '[]',
+                None,
+                '[]',
+                '[]'
+            ))
+
+            #cur.execute("SELECT * FROM Students ORDER BY id DESC LIMIT 1")
+            #new_user = cur.fetchone()
+
+
+            db.commit()
             cur.close()
-            conn.close()
-            return render_template('register.html')
+            db.close()
 
-        # Хеширование пароля
-        password_hash = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+            flash('Регистрация успешна!')
+            return redirect(url_for('login'))
 
-        # Создание пользователя
-        cur.execute(
-            "INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)",
-            (username, email, password_hash)
-        )
+            #return jsonify(new_user)
 
-        conn.commit()
-        cur.close()
-        conn.close()"""
-
-        flash('Регистрация успешна! Теперь вы можете войти.')
-        return redirect(url_for('login'))
+        except Exception as e:
+            print(e)
+            flash('Ошибка при регистрации')
 
     return render_template('register.html')
 
@@ -58,32 +181,105 @@ def login():
         email = request.form['email']
         password = request.form['password']
 
-        """conn = get_db_connection()
-        cur = conn.cursor()
+        try:
+            db = get_database()
+            cur = db.cursor(dictionary=True)
 
-        # Ищем пользователя по email
-        cur.execute(
-            "SELECT id, username, password_hash FROM users WHERE email = ?",
-            (email,)
-        )
-        user = cur.fetchone()
+            # ищем пользователя
+            cur.execute("SELECT * FROM Students WHERE email = %s", (email,))
+            user = cur.fetchone()
 
-        if user and bcrypt.checkpw(password.encode('utf-8'), user[2].encode('utf-8')):
-            session['user_id'] = user[0]
-            session['username'] = user[1]
-            session['email'] = email
+            if user and bcrypt.checkpw(password.encode('utf-8'), user['password_hash'].encode('utf-8')):
+                # сохраняем в сессию
+                session['user_id'] = user['id']
+                session['email'] = user['email']
+                session['name'] = user['first_name']
+
+                cur.close()
+                db.close()
+
+                return redirect(url_for('index'))
+            else:
+                flash('Неверный email или пароль')
+
             cur.close()
-            conn.close()
-            # РЕДИРЕКТ НА ГЛАВНУЮ СТРАНИЦУ (index)
-            return redirect(url_for('index'))
-        else:
-            flash('Неверный email или пароль')
+            db.close()
 
-        cur.close()
-        conn.close()"""
-        return render_template('login.html')
+        except Exception as e:
+            print(e)
+            flash('Ошибка при входе')
 
     return render_template('login.html')
 
+
+
+
+def get_database():
+    return mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='password',
+        database='GetCase'
+    )
+
+def push_to_database(data):
+    db = get_database()
+    cur = db.cursor()
+    command = ("INSERT INTO Companies", "(name,information,projects)")
+    if data[0] == "Students":
+        command = ("INSERT INTO Students", "(skills, university, faculty, course, email, phone_number, tg_id, tasks_started, tasks_progressing, password_hash)", data[1])
+    if data[0] == "Companies":
+        command = ("INSERT INTO Companies", "(name,information,projects)")
+    if data[0] == "Cases":
+        command = ("INSERT INTO Cases", "(organizer_id,performers,description,areas,publication_time,end_time)")
+    else:
+        return
+    cur.execute(command,data[1])
+    db.commit()
+
+def update_database(data):
+    db = get_database()
+    cur = db.cursor()
+    if data[0] == "Students":
+        command = ("UPDATE Students SET skills = %s, university = %s, faculty = %s, course = %s, email = %s, phone_number = %s, tg_id=%s, tasks_started=%s, tasks_progressing=%s, password_hash=%s")
+    if data[0] == "Companies":
+        command = ("UPDATE Companies SET name = %s, information = %s, projects=%s")
+    if data[0] == "Cases":
+        command = "UPDATE Cases SET organizer_Id = %s, performers = %s, description = %s, areas = %s, publication_time = %s, end_time =%s"
+    else:
+        return
+    cur.execute(command, data[1])
+
+
+def pull_database(data):
+    db = get_database()
+    cur = db.cursor()
+    if data[0] != "Students" and data[0] != "Companies" and data[0] != "Cases":
+        return
+    command = ("SELECT * FROM %s WHERE id = %s")
+    cur.execute(command, data)
+    result = cur.fetchall()
+    return result
+
+def date_to_days(date):
+    year = int(date[0:4])
+    month = int(date[5:7])
+    day = int(date[8:10])
+    days = year*364+month*30+day
+    return days
+
+
+def get_cases_from_user(userid):
+    db = get_database()
+    cur = db.cursor()
+    command = ("SELECT tasks_started FROM Students WHERE id = %s")
+    cur.execute(command, userid)
+    result = cur.fetchall()
+    result_sorted = sorted(result, key=lambda k: date_to_days(k['end_time']))
+    return result_sorted
+
+
+
 if __name__ == '__main__':
-    app.run()
+    init_db()
+    app.run(debug=True)
